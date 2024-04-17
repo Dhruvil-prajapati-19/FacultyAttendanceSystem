530d6fb1f039f882c2c598f92877ed6ddc29b118
Dhruvil-prajapati-16 <151436113+Dhruvil-prajapati-16@users.noreply.github.com>
2024-04-17 22:07:45 +0530

    fix js and css for scroling

--- static/js/index.js
+++ static/js/index.js
@@ -2,27 +2,29 @@ const btnR = document.querySelector('.btn-right');
 const btnL = document.querySelector('.btn-left');
 const tracks = document.querySelector('.tracks');
 const tracksW = tracks.scrollWidth;
+let scrollInterval; // Variable to hold the interval for continuous scrolling
 
-btnR.addEventListener('click', _ => {
-  tracks.scrollBy({
-    left: tracksW / 2,
-    behavior: 'smooth'
-  });
+btnR.addEventListener('mousedown', () => {
+  // Start scrolling to the right continuously
+  scrollInterval = setInterval(() => {
+    tracks.scrollBy({
+      left: tracksW / 120, // Adjust the scroll speed as needed
+       // Add smooth behavior
+    });
+  }, 50); // Adjust the interval as needed for the desired scrolling speed
 });
 
-btnL.addEventListener('click', _ => {
-  tracks.scrollBy({
-    left: -tracksW / 2,
-    behavior: 'smooth'
-  });
+btnL.addEventListener('mousedown', () => {
+  // Start scrolling to the left continuously
+  scrollInterval = setInterval(() => {
+    tracks.scrollBy({
+      left: -tracksW / 120, // Adjust the scroll speed as needed
+     // Add smooth behavior
+    });
+  }, 50); // Adjust the interval as needed for the desired scrolling speed
 });
 
-
-let observer = new IntersectionObserver(entries => {
-  if (!entries[0].isIntersecting) {
-    document.body.classList.add("reveal");
-  } else {
-    document.body.classList.remove("reveal");
-  }
+// Stop scrolling when the mouse button is released
+document.addEventListener('mouseup', () => {
+  clearInterval(scrollInterval);
 });
-observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
