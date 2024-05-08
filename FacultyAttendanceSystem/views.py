670274a8f3193226b77ba3fceb3935a32be60a0c
Dhruvil-prajapati-16 <151436113+Dhruvil-prajapati-16@users.noreply.github.com>
2024-05-08 18:04:41 +0530

    changes by teamates

--- FacultyAttendanceSystem/views.py
+++ FacultyAttendanceSystem/views.py
@@ -69,7 +69,7 @@ class LoginView(View):
             if admin_credentials.password == password:
                 logged_user = admin_credentials.faculty.id
                 request.session['logged_user'] = logged_user
-                messages.success(request, f'You have successfully logged in. Your ID is {logged_user}')
+                messages.success(request, f'You have successfully logged in as {admin_credentials.faculty}')
                 return redirect('index/')
             else:
                 error_message = 'Invalid password'
